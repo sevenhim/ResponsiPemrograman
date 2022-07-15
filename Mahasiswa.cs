27ef7a5de7d6ecd4b26a0071f7af076748538b7f
sevenhim <101778766+sevenhim@users.noreply.github.com>
2022-07-15 10:32:53 +0700

    responsi

---
+++ Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+
+        // properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Gender { get; set; }
+        public string Ipk { get; set; }
+    }
+}
